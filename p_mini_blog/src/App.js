5ee24534c1a218f0773405f07f44a6793d0bfa51
Davi Silva Santos <morg.daviss@gmail.com>
2022-11-07 21:05:57 -0300

    mini blog day 2

--- p_mini_blog/src/App.js
+++ p_mini_blog/src/App.js
@@ -1,34 +1,81 @@
-import{BrowserRouter, Routes, Route, Navigate} from "react-router-dom";
+// CSS
 import './App.css';
+
+import{BrowserRouter, Routes, Route, Navigate} from "react-router-dom";
+import { onAuthStateChanged } from 'firebase/auth';
+
+// context
+import { AuthProvider } from "./context/AuthContext";
+
+// hooks
+import {useState, useEffect} from "react";
+
+// custom hooks;
+import { useAuthentication } from './hooks/useAuthentication';
 
 // Pages Import
 import Home from "./pages/Home/Home";
 import About from "./pages/About/About";
 import Login from "./pages/Login/Login";
 import Register from "./pages/Register/Register";
+import CreatePost from './pages/CreatePost/CreatePost';
+import Dashboard from './pages/Dashboard/Dashboard';
 
 // Components Import
 import Navbar from "./components/Navbar";
 import Footer from "./components/Footer";
 
 
+
 function App() {
+  const [user, setUser] = useState(undefined);
+  const {auth} = useAuthentication(); 
+
+  const loadingUser = user === undefined;
+
+  useEffect(()=>{
+    onAuthStateChanged(auth, (user)=>{
+      setUser(user)
+    })
+  },[auth])
+
+  if(loadingUser){
+    return <p>Carregando...</p>
+  }
+
+
   return (
     <div className="App">
-      <BrowserRouter>
-        <Navbar />
-          <div className="container">
-            <Routes>
-              <Route>
-                <Route path="/" element={<Home />} />
-                <Route path="/about" element={<About />} />
-                <Route path="/login" element={<Login />} />
-                <Route path="/register" element={<Register />} />
-              </Route>
-            </Routes>
-          </div>
-        <Footer />
-      </BrowserRouter>
+      <AuthProvider value={{user}}>
+        <BrowserRouter>
+          <Navbar />
+            <div className="container">
+              <Routes>
+                <Route>
+                  <Route path="/" element={<Home />} />
+                  <Route path="/about" element={<About />} />
+                  <Route
+                    path="/login"
+                    element={!user ? <Login /> : <Navigate to="/" />}
+                  />
+                  <Route 
+                    path="/register" 
+                    element={!user ? <Register /> : <Navigate to="/" />} 
+                  />
+                  <Route 
+                    path="/posts/create" 
+                    element={user ? <CreatePost /> : <Navigate to="/login" />} 
+                  />
+                  <Route 
+                    path="/dashboard" 
+                    element={user ? <Dashboard /> : <Navigate to="/login" />} 
+                  />
+                </Route>
+              </Routes>
+            </div>
+          <Footer />
+        </BrowserRouter>
+      </AuthProvider>
     </div>
   );
 }
--- p_mini_blog/src/pages/About/About.js
+++ p_mini_blog/src/pages/About/About.js
@@ -1,9 +1,16 @@
 // CSS
 import styles from "./About.module.css";
+import {Link} from "react-router-dom";
 
 const About = () => {
   return (
-    <div>About</div>
+    <div className={styles.about}>
+      <h2>Sobre o Mini <span>Blog</span></h2>
+      <p>Esse projeto consiste num blog feito com React no front-end e Firebase no back-end.</p>
+      <Link to="/posts/create" className="btn">
+        Criar Post!
+      </Link>
+    </div>
   )
 }
 
